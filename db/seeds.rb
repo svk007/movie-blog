78e313e4192f7959c4351b4337d3664d14f542c4
Souvik Das <souvikdas728@gmail.com>
2016-06-22 14:40:14 +0530

    Added endless scroll and seeded random pretty data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..120).each do |i|
+  Post.create!(name: "Lipsum Post #{i}", review: %{
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat purus dapibus fermentum},
+      image: File.new("#{Rails.root}/Images/#{i}.jpg"), year: "#{1950+i}")
+end
